9055b19daa6a5ab59698bf41c3591ac30ae82c7e
Juan Da Silva <jcda_23@hotmail.com>
2022-09-22 21:27:47 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  data: any[] = [];
   title = 'componentes-juan-dasilva';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://api.spacexdata.com/v4/rockets')
+      .subscribe((data: any) => {
+        this.data = data;
+      });
+  }
 }
